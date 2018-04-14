003cbcc6dddfbfabc56e3fa40108e4afc0fb8346
zicova-pl <zicova.pl@gmail.com>
2018-04-14 18:05:39 +0800

    Models for FD, Payment and Transaction

---
+++ database/migrations/2018_04_14_093644_create_fixed_deposits_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFixedDepositsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fixed_deposits', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('account_no');
+            $table->string('account_type');
+            $table->integer('balance');
+            $table->integer('tenure_duration');
+            $table->char('currency',3);
+            $table->float('current_balance');
+            $table->flaot('available_balance');
+            $table->date('maturity_date');
+            $table->float('investment_rate');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fixed_deposits');
+    }
+}
---
+++ database/migrations/2018_04_14_093758_create_transactions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->date('transaction_date');
+            $table->string('description');
+            $table->float('debit_amount');
+            $table->float('credit_amount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
