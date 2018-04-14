7f5ebb71cbc12c16c7cdcf35a6555a19455fe304
Frango <frango_95@hotmail.com>
2018-04-14 23:18:06 +0800

    profileFactory

--- database/factories/ProfileFactory.php
+++ database/factories/ProfileFactory.php
@@ -4,6 +4,11 @@ use Faker\Generator as Faker;
 
 $factory->define(Model::class, function (Faker $faker) {
     return [
-        //
+      'gender' => $faker->text(1),
+      'first_name' => $faker->text(5),
+      'last_name' => $faker->text(5),
+      'age' => $faker->numberBetween($min = 10, $max = 60),
+      'address' =>$faker->text(30),
+      'country' => $faker->text(8),
     ];
 });
